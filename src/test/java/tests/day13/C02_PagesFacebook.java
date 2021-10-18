b551df33d50a8197296b475d6286f85dcb125c9c
Muhammed Solmazgul <mmsolmazgul039@gmail.com>
2021-10-18 22:21:43 +0200

    AmazonPage

---
+++ src/test/java/tests/day13/C02_PagesFacebook.java
@@ -0,0 +1,31 @@
+package tests.day13;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.TestBase;
+
+public class C02_PagesFacebook extends TestBase {
+    @Test
+    public void test() {
+        //// facebook login sayfasina gidin
+
+        driver.get("https://www.facebook.com");
+        FacebookPage facebookPage=new FacebookPage(driver);
+
+        facebookPage.cookiesgec();
+        //// kullanici adi : techproed@hotmail.com
+        facebookPage.mailKutusu.sendKeys("techproed@hotmail.com");
+        //// sifre : 12345
+        facebookPage.sifreKutusu.sendKeys("12345");
+        facebookPage.loginTusu.click();
+        //// yazdigimizda giris yapamadigimizi test edin
+        Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
+        //page sayfasinda cookiesGec() method u olusturun
+
+
+
+    }
+
+
+}
